b1ca0c1cddb6c0d2757af271a99739e84f5533a5
Gaeulbyul <aeiou256@gmail.com>
2022-05-29 16:27:16 +0900

    우클릭메뉴 t.co 링크 처리

--- src/background/context-menus.ts
+++ src/background/context-menus.ts
@@ -18,7 +18,10 @@ const targetUrlPatterns = [
   'https://rationalwiki.org/wiki/*',
   'https://github.com/*',
   'https://*.github.io/*',
+  'https://t.co/*',
 ]
+
+const tcoCache = new Map<string, string>()
 
 async function manuallyIdentify(identifier: string, group: RedEyesFilterGroup) {
   const { manuallyIdentified } = await RedEyesStorage.loadLocalStorageOnly('manuallyIdentified')
@@ -28,18 +31,44 @@ async function manuallyIdentify(identifier: string, group: RedEyesFilterGroup) {
   })
 }
 
-browser.contextMenus.onClicked.addListener((clickInfo, tab) => {
-  const { linkUrl } = clickInfo
-  if (!(linkUrl && tab)) {
-    return
-  }
+async function resolveTwitterShortLink(url: URL) {
+  const responseText = await fetch(url.toString(), {
+    method: 'get',
+    referrer: 'https://twitter.com/home',
+  }).then(resp => resp.text())
+  // Manifest v3에선 DOMParser등 DOM 관련 API를 사용할 수 없다.
+  // 따라서 그냥 정규식을 사용한다.
+  // meta http-equiv=redirect나 location.replace(...)에서 URL을 가져올 것.
+  const match = /;URL=([^\x22]+)/.exec(responseText)
+  return match ? match[1] : null
+}
+
+async function identifyViaContextMenu(clickInfo: browser.Menus.OnClickData, tab: browser.Tabs.Tab) {
+  const linkUrl = new URL(clickInfo.linkUrl!)
   const tabId = tab.id!
-  const identifier = getIdentifier(linkUrl)
+
+  let tcoResolvedLinkUrl: string | null = null
+  if (linkUrl.hostname === 't.co') {
+    const tcoPath = linkUrl.pathname.slice(1)
+    if (tcoCache.has(tcoPath)) {
+      tcoResolvedLinkUrl = tcoCache.get(tcoPath)!
+    } else {
+      tcoResolvedLinkUrl = await resolveTwitterShortLink(linkUrl)
+      if (tcoResolvedLinkUrl) {
+        tcoCache.set(tcoPath, tcoResolvedLinkUrl)
+      }
+    }
+  }
+
+  const identifier = getIdentifier(tcoResolvedLinkUrl || linkUrl)
   if (!identifier) {
     const msg: REMessageToContent.Alert = {
       messageTo: 'content',
       messageType: 'Alert',
       text: `RedEyes can't identify such url: "${linkUrl}"`,
+    }
+    if (tcoResolvedLinkUrl) {
+      msg.text += ` / (t.co)"${tcoResolvedLinkUrl}"`
     }
     browser.tabs.sendMessage(tabId, msg)
     return
@@ -63,6 +92,16 @@ browser.contextMenus.onClicked.addListener((clickInfo, tab) => {
       manuallyIdentify(identifier, 'neutral')
       browser.tabs.sendMessage(tabId, { ...repaintMsg, group: 'neutral' })
       break
+  }
+}
+
+browser.contextMenus.onClicked.addListener((clickInfo, tab) => {
+  const { linkUrl } = clickInfo
+  if (!(linkUrl && tab)) {
+    return
+  }
+  if (/^manually-identify-as-/.test(clickInfo.menuItemId.toString())) {
+    identifyViaContextMenu(clickInfo, tab)
   }
 })
 
